1a6d83170435c5c53895d57a91e03f8059ae6230
Julio Foulquie <jfoulquie@gmail.com>
2015-11-29 15:57:00 -0300

    Add CommandServiceProvider to add commands

---
+++ app/Providers/CommandServiceProvider.php
@@ -0,0 +1,19 @@
+<?php namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use App\Console\Commands\UserStream;
+use App\Console\Commands\CliGame;
+
+class CommandServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
