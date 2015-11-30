53c5905c10169b744bcde9355dbbc4c2a93bcc62
Julio Foulquie <jfoulquie@gmail.com>
2015-11-29 22:12:01 -0300

    Add twitter config file and keys to .env.example

---
+++ config/twitter.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    '0003Julio' => [
+        'consumer_key'      => env("CONSUMERKEY", 'YOUR_CONSUMER_KEY'),
+        'consumer_secret'   => env("CONSUMERSECRET", 'YOUR_CONSUMER_SECRET'),
+        'token'             => env("0003Julio.TOKEN", 'A_USER_TOKEN'),
+        'secret'            => env("0003Julio.SECRET", 'A_USER_TOKEN_SECRET'),
+    ],
+];
